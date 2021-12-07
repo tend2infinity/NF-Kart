e5c14cde1ea78fa9bb8e2b9081e3742a0c91d75a
Kumar Lakshya <klakshya170@gmail.com>
2021-12-07 14:12:10 +0530

    adding navbar and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
 import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Container } from "react-bootstrap"
 function App() {
-  return <div className='App'>Welcome to Proshop</div>
+  return (
+    <div className='App'>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
