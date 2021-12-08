740831f184a65a7f6e4762ee56f05f0de633cdf9
Kumar Lakshya <klakshya170@gmail.com>
2021-12-08 15:09:09 +0530

    Adding the products list int the main folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/HomeScreen"
 function App() {
   return (
     <div className='App'>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Proshop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
