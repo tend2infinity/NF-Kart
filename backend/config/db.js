cb46208a029ff64bc77467586742d45d297c5b9e
Kumar Lakshya <klakshya170@gmail.com>
2021-12-15 12:38:12 +0530

    Modeling the data

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     })
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.log(`Error : ${error.message}`)
+    console.log(`Error : ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
