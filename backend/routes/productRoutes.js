758d9c0d11e841c1e906db12c5224ce31d3d55a1
Kumar Lakshya <klakshya170@gmail.com>
2022-07-14 04:11:16 +0530

    implementing backend user authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,18 @@
 import express from "express"
-import AsyncHandler from "express-async-handler"
 const router = express.Router()
-import Product from "../models/productModel.js"
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js"
 
 //@desc   Fetch all products
 //@route GET /api/products
 //@access Public
-router.get(
-  "/",
-  AsyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
+router.route("./").get(getProducts)
 
 //@desc   Fetch single products
 //@route GET /api/products/:id
 //@access Public
-router.get(
-  "/:id",
-  AsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error("Product not found")
-    }
-  })
-)
+router.route("/:id").get(getProductById)
 
 export default router
